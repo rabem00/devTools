8c156c1ab06037431028d86b3575ad968ada043d
Marco Rabelink <marco.rabelink@gmail.com>
2023-04-03 22:00:02 +0200

    Add requirements.txt for pip install

---
+++ README.md
@@ -0,0 +1,2 @@
+# Development Tooling
+Just a repository with some tools and scripts/programs to use during development.
--- setup-conda.sh
+++ setup-conda.sh
@@ -6,4 +6,4 @@ read -rp "Enter python version (e.g. 3.10.9) " python_version
 conda create -yn "$env_name" python="$python_version"
 conda activate "$env_name"
 
-
+pip3 install -r requirements.txt
