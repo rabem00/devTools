cf6da1cb2a68375ede8e09cb6d680f28a2bed376
Marco Rabelink <marco.rabelink@gmail.com>
2023-04-03 21:53:06 +0200

    first commit

---
+++ setup-conda.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# create conda env
+read -rp "Enter environment name: " env_name
+read -rp "Enter python version (e.g. 3.10.9) " python_version
+conda create -yn "$env_name" python="$python_version"
+conda activate "$env_name"
+
+
